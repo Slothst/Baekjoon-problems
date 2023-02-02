9655aca89a61245f57d8dc0a9e21830a0742c247
Gawq <cnj991279@gmail.com>
2023-02-02 16:41:42 +0900

    10869

---
+++ Java/10869/_10869.java
@@ -0,0 +1,16 @@
+public class _10869 {
+    static int A, B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}
