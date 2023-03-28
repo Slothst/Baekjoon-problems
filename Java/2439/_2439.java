f8cf9f3d6d9c91669917813a25bc7c888c39c19b
Gawq <cnj991279@gmail.com>
2023-03-28 21:13:01 +0900

    2439

---
+++ Java/2439/_2439.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class _2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = N - 1; j >= 0; j--) {
+                if (i < j) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
