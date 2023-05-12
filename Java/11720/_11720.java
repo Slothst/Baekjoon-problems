43c425c550dab81adf455d3e196acdbba83ddc28
aaa <cnj991279@gmail.com>
2023-05-12 20:14:38 +0900

    11720

---
+++ Java/11720/_11720.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class _11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String s = br.readLine();
+        String[] arr = s.split("");
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(arr[i]);
+        }
+
+        System.out.println(sum);
+    }
+}
