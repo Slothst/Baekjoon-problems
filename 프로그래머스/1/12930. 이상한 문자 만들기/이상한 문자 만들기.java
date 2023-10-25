56f0b16fa86ac19ada088d394e07c139aedc7d7a
Slothst <102890326+Slothst@users.noreply.github.com>
2023-10-25 16:59:48 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -2,23 +2,16 @@ import java.util.regex.Pattern;
 
 class Solution {
     public String solution(String s) {
-        String[] split = s.split(" ", -1);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < split.length; i++) {
-            char[] charArray = split[i].toCharArray();
-            for (int j = 0; j < split[i].length(); j++) {
-                if (j % 2 == 0) {
-                    charArray[j] = Character.toUpperCase(charArray[j]);
-                } else {
-                    charArray[j] = Character.toLowerCase(charArray[j]);
-                }
-            }
-            if (i == split.length - 1) {
-                sb.append(charArray);
+        char[] arr = s.toCharArray();
+        int idx = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ') {
+                idx = 0;
             } else {
-                sb.append(charArray).append(" ");
+                arr[i] = (idx++ % 2 == 0 ? Character.toUpperCase(arr[i]) : Character.toLowerCase(arr[i]));
             }
         }
-        return sb.toString();
+        return String.valueOf(arr);
     }
 }
