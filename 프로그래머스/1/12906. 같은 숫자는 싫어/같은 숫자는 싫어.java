49faa7faed93ec9923f462abcc00cb01742314d2
Slothst <102890326+Slothst@users.noreply.github.com>
2023-10-25 15:18:25 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.24 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -6,10 +6,10 @@ public class Solution {
     public int[] solution(int []arr) {
         List<Integer> list = new ArrayList<>();
         int value = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != value) {
-                list.add(arr[i]);
-                value = arr[i];
+        for (int i : arr) {
+            if (i != value) {
+                list.add(i);
+                value = i;
             }
         }
         return list.stream().mapToInt(Integer::intValue).toArray();
