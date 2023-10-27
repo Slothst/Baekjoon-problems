ac66156a571b5ea47166ee8208cedd4ee244a027
Slothst <102890326+Slothst@users.noreply.github.com>
2023-10-27 17:51:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.34 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,25 +4,8 @@ class Solution {
     final static List<String> list = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
     public int solution(String s) {
         for (int i = 0; i < list.size(); i++) {
-            s = changeString(s, i);
+            s = s.replaceAll(list.get(i), Integer.toString(i));
         }
         return Integer.parseInt(s);
-    }
-    
-    public static String changeString(String s, int i) {
-        boolean isAllDigit = true;
-        for (char c : s.toCharArray()) {
-            if (!Character.isDigit(c)) {
-                isAllDigit = false;
-                break;
-            }
-        }
-        if (isAllDigit) {
-            return s;
-        }
-        if (s.contains(list.get(i))) {
-            s = s.replaceAll(list.get(i), String.valueOf(i));
-        }
-        return s;
     }
 }   
