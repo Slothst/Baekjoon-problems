6e65d1cff6337ec3a9045201d208ab75d81f7934
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-07 22:48:30 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.swift
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ s1:[String], _ s2:[String]) -> Int {
-    var count = 0
-    for s in s1 {
-        if s2.contains(s) {
-            count += 1
-        }
-    }
-    return count
+    return Set(s1).intersection(Set(s2)).count
 }
