685a2f231f7e5d86abd019dd9b671faa6ea386f9
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-07 23:19:39 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    var answer = [Int]()
-    var a = 0, b = 0
-    for num in num_list {
-        if num % 2 == 0 {
-            a += 1
-        } else {
-            b += 1
-        }
-    }
-    answer.append(a)
-    answer.append(b)
-    return answer
+    return [num_list.filter { $0 % 2 == 0}.count, num_list.filter { $0 % 2 == 1}.count]
 }
