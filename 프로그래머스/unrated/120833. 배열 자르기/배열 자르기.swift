6af708e372401d45b00006fd9969f16f20c6ca51
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-08 15:08:40 +0900

    [level unrated] Title: 배열 자르기, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.swift
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    var n = [Int]()
-    for num in numbers[num1...num2] {
-        n.append(num)
-    }
-    return n
+    return (num1...num2).map { numbers[$0] }
 }
