0b37ac8ba5203da99b11219e96b36bc4876da257
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-08 16:24:56 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.swift
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    return Array(num_list[0..<n])
+    // return Array(num_list[0..<n])
+    return Array(num_list.prefix(n))
 }
