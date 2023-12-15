8f958aacb284fbd2711c5d69dc479b136b6bdfb3
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-15 13:11:41 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -1,13 +1,11 @@
 import Foundation
 
 func solution(_ dot:[Int]) -> Int {
-    if dot[0] > 0 && dot[1] > 0 {
-        return 1
-    } else if dot[0] < 0 && dot[1] > 0 {
-        return 2
-    } else if dot[0] < 0 && dot[1] < 0 {
-        return 3
-    } else {
-        return 4
+    switch (dot[0], dot[1]) {
+        case(0..., 0...): return 1
+        case(...0, 0...): return 2
+        case(...0, ...0): return 3
+        case(0..., ...0): return 4
+        default: return 0
     }
 }
