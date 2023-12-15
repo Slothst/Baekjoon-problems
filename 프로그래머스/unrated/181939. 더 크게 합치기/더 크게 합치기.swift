84ecd07d9bc3bdb8ca7104ef583d8fba203bc641
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-15 14:51:20 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.swift
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int) -> Int {
-    return String(a) + String(b) > String(b) + String(a) ? Int(String(a) + String(b))! : Int(String(b) + String(a))!
+    max(Int(String(a) + String(b))!, Int(String(b) + String(a))!)
 }
