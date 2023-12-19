280efa49f975b9df9942c3582859573b6f999e86
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-19 10:26:32 +0900

    [level unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ arr:[Int], _ n:Int) -> [Int] {
+    var result = arr
+    if arr.count % 2 == 1 {
+        for i in arr.indices where i % 2 == 0 {
+            result[i] += n
+        }
+    } else {
+        for i in arr.indices where i % 2 == 1 {
+            result[i] += n
+        }
+    }
+    return result
+}
