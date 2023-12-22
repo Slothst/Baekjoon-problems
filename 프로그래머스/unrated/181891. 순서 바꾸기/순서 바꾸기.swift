2b65c9dbffda581474f846cb8ef9409856f8af02
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-22 11:29:18 +0900

    [level unrated] Title: 순서 바꾸기, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.swift
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.swift
@@ -1,8 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    var answer = [Int]()
-    answer = Array(num_list[n...])
-    answer.append(contentsOf: num_list[0..<n])
-    return answer
+    return Array(num_list[n...]) + Array(num_list[..<n])
 }
