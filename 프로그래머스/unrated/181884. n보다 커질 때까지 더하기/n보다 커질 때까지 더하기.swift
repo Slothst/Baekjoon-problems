ad11ebad547a33c559de39932ec975915392e331
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2023-12-22 11:17:58 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ n:Int) -> Int {
-    var sum = 0
-    for i in numbers {
-        if sum > n {
-            return sum
-        }
-        sum += i
-    }
-    return sum
+    return numbers.reduce(0) { $0 > n ? $0 : $0 + $1 }
 }
