3905cc893e83f6d0b0adaa5e93cf57096ed09abd
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-01 15:39:11 +0900

    [level unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ myString:String) -> String {
-    return myString.map { $0 == "a" || $0 == "A" ? $0.uppercased() : $0.lowercased() }.joined()
+    // return myString.map { $0 == "a" || $0 == "A" ? $0.uppercased() : $0.lowercased() }.joined()
+    return myString.lowercased().replacingOccurrences(of: "a", with: "A")
 }
