dab3cf8346a946766d5f5af0e49bbad156ae7883
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-02 16:52:00 +0900

    [level unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ n_str:String) -> String {
+    var array = Array(n_str)
+    if array[0] != "0" {
+        return n_str
+    }
+
+    while true {
+        if array[0] == "0" {
+            array.removeFirst()
+            continue
+        }
+        break;
+    }
+
+    return array.map { String($0) }.joined()
+}
