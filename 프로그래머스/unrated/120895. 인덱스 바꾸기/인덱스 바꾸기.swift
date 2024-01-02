49831ddde48fcd2b8770f84d3beb9d63146242dc
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-02 16:32:42 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
+    var array = Array(my_string)
+    var temp = array[num1]
+    array[num1] = array[num2]
+    array[num2] = temp
+    return String(array)
+}
