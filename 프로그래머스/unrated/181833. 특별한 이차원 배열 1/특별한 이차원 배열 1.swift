7da1b76b42623dc4f2b2e7ab76ba2215edd63805
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-04 11:02:52 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 1.47 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.swift
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.swift
@@ -1,13 +1,15 @@
 import Foundation
 
 func solution(_ n:Int) -> [[Int]] {
-    var arr: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
-    for i in 0..<n {
-        for j in 0..<n {
-            if i == j {
-                arr[i][j] = 1
-            }
-        }
-    }
-    return arr
+    // var arr: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
+    // for i in 0..<n {
+    //     for j in 0..<n {
+    //         if i == j {
+    //             arr[i][j] = 1
+    //         }
+    //     }
+    // }
+    // return arr
+    
+    return (0..<n).map { i in (0..<n).map { j in i == j ? 1 : 0 } }
 }
