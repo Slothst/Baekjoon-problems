61c2e13a863c7cf597b3826646e03e32d50f267d
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-04 11:16:12 +0900

    [level unrated] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.swift
+++ 프로그래머스/unrated/120844. 배열 회전시키기/배열 회전시키기.swift
@@ -3,14 +3,16 @@ import Foundation
 func solution(_ numbers:[Int], _ direction:String) -> [Int] {
     var arr = numbers
     
-    if direction == "right" {
-        var temp = arr[arr.count - 1]
-        arr.remove(at: arr.count - 1)
-        arr.insert(temp, at: 0)
-    } else {
-        var temp = arr[0]
-        arr.remove(at: 0)
-        arr.append(temp)
-    }
+    // if direction == "right" {
+    //     var temp = arr[arr.count - 1]
+    //     arr.remove(at: arr.count - 1)
+    //     arr.insert(temp, at: 0)
+    // } else {
+    //     var temp = arr[0]
+    //     arr.remove(at: 0)
+    //     arr.append(temp)
+    // }
+    
+    direction == "right" ? arr.insert(arr.removeLast(), at: 0) : arr.append(arr.removeFirst())
     return arr
 }
