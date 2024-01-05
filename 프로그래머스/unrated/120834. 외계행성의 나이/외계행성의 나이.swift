13b2c8042a9d4650bf9f8891d7960b2627198d1d
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-05 14:35:10 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.swift
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.swift
@@ -1,9 +1,6 @@
 import Foundation
 
 func solution(_ age:Int) -> String {
-    var answer = [String]()
     var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-    String(age).map { answer.append(alpha[Int(String($0))!]) }
-    
-    return answer.joined()
+    return String(age).map { alpha[Int(String($0))!] }.joined()
 }
