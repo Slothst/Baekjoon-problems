317a92918e5251c92c0a3f56f301b50e4ffe9ea5
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-05 14:41:56 +0900

    [level unrated] Title: 문자열 잘라서 정렬하기, Time: 56.15 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.swift
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 func solution(_ myString:String) -> [String] {
-    return myString.components(separatedBy: "x").filter { $0 != "" }.sorted()
+    // return myString.components(separatedBy: "x").filter { $0 != "" }.sorted()
+    return myString.split(separator: "x").map { String($0) }.sorted()
 }
