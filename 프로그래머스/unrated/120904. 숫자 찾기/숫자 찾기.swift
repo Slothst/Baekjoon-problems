4b2530cefcdbaf29d015c6313c3cf710be2371fb
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-06 11:04:41 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.swift
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.swift
@@ -1,17 +1,5 @@
 import Foundation
 
 func solution(_ num:Int, _ k:Int) -> Int {
-    let nums = String(num).map { String($0) }
-        
-    if nums.contains(String(k)) == false {
-        return -1
-    }
-
-    for i in 0..<nums.count {
-        if nums[i] == String(k) {
-            return i+1
-        }
-    }
-
-    return -1
+    Array("-\(num)").firstIndex(of: Character(String(k))) ?? -1
 }
