732417b7cdf1d277db4946be1482271444b46ff6
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-08 11:17:05 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 13.38 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+    var result = arr
+    for i in 0...queries.count - 1 {
+        result[queries[i][0]...queries[i][1]] = ArraySlice(result[queries[i][0]...queries[i][1]].map { $0 + 1 })
+    }
+    return result
+}
