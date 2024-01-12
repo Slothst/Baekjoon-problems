f86851a7b230771792b02ac76262e39c5bb1061b
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-12 12:22:40 +0900

    [level Lv.0] Title: A로 B 만들기, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120886. A로 B 만들기/A로 B 만들기.swift
+++ 프로그래머스/Lv.0/120886. A로 B 만들기/A로 B 만들기.swift
@@ -1,8 +1,5 @@
 import Foundation
 
 func solution(_ before:String, _ after:String) -> Int {
-    var arr1 = Array(before).sorted()
-    var arr2 = Array(after).sorted()
-    
-    return arr1 == arr2 ? 1 : 0
+    return Array(before).sorted() == Array(after).sorted() ? 1 : 0
 }
