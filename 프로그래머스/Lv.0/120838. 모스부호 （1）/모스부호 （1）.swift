c6fd5d68d06fd9a65ec3389c333c662b3dd7ecc6
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-12 13:18:44 +0900

    [level Lv.0] Title: 모스부호 (1), Time: 0.35 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120838. 모스부호 （1）/모스부호 （1）.swift
+++ 프로그래머스/Lv.0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -1,18 +1,12 @@
 import Foundation
 
 func solution(_ letter:String) -> String {
-    let morse: [String: Character] = [
+    let morse: [String: String] = [
     ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f",
     "--.": "g", "....": "h", "..": "i", ".---": "j", "-.-": "k", ".-..": "l",
     "--": "m", "-.": "n", "---": "o", ".--.": "p", "--.-": "q", ".-.": "r",
     "...": "s", "-": "t", "..-": "u", "...-": "v", ".--": "w", "-..-": "x",
     "-.--": "y", "--..": "z"
     ]
-    var letter = letter.components(separatedBy: " ")
-    var result = [Character]()
-    for el in letter {
-        result.append(morse[el] ?? " ")
-    }
-
-    return String(result)
+    return letter.components(separatedBy: " ").map { morse[$0] ?? "" }.reduce("", +)
 }
