de835a1a9610ed6c24a19893138de9b0b7ed7cbd
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-12 13:03:05 +0900

    [level Lv.0] Title: k의 개수, Time: 31.84 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120887. k의 개수/k의 개수.swift
+++ 프로그래머스/Lv.0/120887. k의 개수/k의 개수.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
-    var count = 0
-
-    for n in i...j where String(n).contains(String(k)) {
-        var arr = Array(String(n))
-        for el in arr where el == Character(String(k)) {
-            count += 1
-        }
-    }
-    return count
+    (i...j).map { String($0).filter { String($0) == String(k) }.count }.reduce(0, +)
 }
