6b5cc9205b7d9b3e31e59ac35b756007585df6ce
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-15 12:21:02 +0900

    [level Lv.0] Title: 진료 순서 정하기, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.swift
+++ 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -1,18 +1,19 @@
 import Foundation
 
 func solution(_ emergency:[Int]) -> [Int] {
-    var arr = emergency.enumerated().sorted {
-        $0.element > $1.element
-    }
+//     var arr = emergency.enumerated().sorted {
+//         $0.element > $1.element
+//     }
 
-    var result = [Int](repeating: 0, count: emergency.count)
+//     var result = [Int](repeating: 0, count: emergency.count)
 
-    for i in 0..<arr.count {
-        for j in 0..<arr.count {
-            if arr[i].element == emergency[j] {
-                result[j] = i + 1
-            }
-        }
-    }
-    return result
+//     for i in 0..<arr.count {
+//         for j in 0..<arr.count {
+//             if arr[i].element == emergency[j] {
+//                 result[j] = i + 1
+//             }
+//         }
+//     }
+//     return result
+    return emergency.map { emergency.sorted(by: >).firstIndex(of: $0)! + 1 }
 }
