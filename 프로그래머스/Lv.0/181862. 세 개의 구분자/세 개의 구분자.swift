4be9fcc8f4416224a1360f8e07ce0010f299b987
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-15 13:12:09 +0900

    [level Lv.0] Title: 세 개의 구분자, Time: 108.90 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181862. 세 개의 구분자/세 개의 구분자.swift
+++ 프로그래머스/Lv.0/181862. 세 개의 구분자/세 개의 구분자.swift
@@ -1,23 +1,27 @@
 import Foundation
 
 func solution(_ myStr:String) -> [String] {
-    var result = [String]()
-    var str = ""
-    for i in Array(myStr) {
-        if (i == "a" || i == "b" || i == "c") && str != "" {
-            result.append(str)
-            str = ""
-            continue
-        }
-        else if i == "a" || i == "b" || i == "c" {
-            continue
-        }
-        str += String(i)
-    }
+//     var result = [String]()
+//     var str = ""
+//     for i in Array(myStr) {
+//         if (i == "a" || i == "b" || i == "c") && str != "" {
+//             result.append(str)
+//             str = ""
+//             continue
+//         }
+//         else if i == "a" || i == "b" || i == "c" {
+//             continue
+//         }
+//         str += String(i)
+//     }
     
-    if str != "" {
-        result.append(str)    
-    }
+//     if str != "" {
+//         result.append(str)    
+//     }
+    
+//     return result.isEmpty ? ["EMPTY"] : result
+    
+    var result = myStr.split { $0 == "a" || $0 == "b" || $0 == "c" }.map { String($0) }
     
     return result.isEmpty ? ["EMPTY"] : result
 }
