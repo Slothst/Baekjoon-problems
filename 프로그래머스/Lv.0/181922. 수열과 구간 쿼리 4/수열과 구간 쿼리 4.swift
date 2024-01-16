357c2eb423ed6496066a261b58de173a2e357374
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-16 13:53:05 +0900

    [level Lv.0] Title: 수열과 구간 쿼리 4, Time: 60.05 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
+++ 프로그래머스/Lv.0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -2,14 +2,18 @@ import Foundation
 
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var arr = arr
-    var result = [Int]()
+//     var result = [Int]()
     
-    for query in queries {
-        arr[query[0]...query[1]].indices.filter { $0 % query[2] == 0 }.map { result.append(Int($0)) }
-        for i in result {
-            arr[i] += 1
-        }
-        result.removeAll()
+//     for query in queries {
+//         arr[query[0]...query[1]].indices.filter { $0 % query[2] == 0 }.map { result.append(Int($0)) }
+//         for i in result {
+//             arr[i] += 1
+//         }
+//         result.removeAll()
+//     }
+    queries.forEach {
+        let (s, e, k) = ($0[0], $0[1], $0[2])
+        (s...e).forEach { if $0 % k == 0 { arr[$0] += 1 } }
     }
     return arr
 }
