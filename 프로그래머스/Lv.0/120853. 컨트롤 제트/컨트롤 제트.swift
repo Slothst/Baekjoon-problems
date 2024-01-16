58b9fbbd808cb75d2dcc6e2c8340bcc2b80ef503
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-16 14:32:22 +0900

    [level Lv.0] Title: 컨트롤 제트, Time: 0.23 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120853. 컨트롤 제트/컨트롤 제트.swift
+++ 프로그래머스/Lv.0/120853. 컨트롤 제트/컨트롤 제트.swift
@@ -1,16 +1,26 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    var arr = s.components(separatedBy: .whitespaces)
-    var i = 0
-    while i < arr.count {
-        if arr[i] == "Z" {
-            arr.remove(at: i)
-            arr.remove(at: i - 1)
-            i = 0
-            continue
+    // var arr = s.components(separatedBy: .whitespaces)
+    // var i = 0
+    // while i < arr.count {
+    //     if arr[i] == "Z" {
+    //         arr.remove(at: i)
+    //         arr.remove(at: i - 1)
+    //         i = 0
+    //         continue
+    //     }
+    //     i += 1
+    // }
+    // return arr.map { Int($0)! }.reduce(0, +) 
+    var stack = [Int]()
+    
+    for w in s.components(separatedBy: .whitespaces) {
+        if w == "Z" {
+            stack.popLast()
+        } else {
+            stack.append(Int(w)!)
         }
-        i += 1
     }
-    return arr.map { Int($0)! }.reduce(0, +) 
+    return stack.reduce(0, +)
 }
