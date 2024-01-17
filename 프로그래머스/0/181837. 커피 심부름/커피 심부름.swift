030726facb6d1d73ea13145be69e90cea1350536
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-17 12:06:13 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.swift
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 func solution(_ order:[String]) -> Int {
-        var menu = ["americano" : 4500, "iceamericano" : 4500, "americanoice" : 4500, "hotamericano" : 4500, "americanohot" : 4500, "cafelatte" : 5000, "icecafelatte" : 5000, "cafelatteice" : 5000, "hotcafelatte" : 5000, "cafelattehot" : 5000, "anything" : 4500]
+//     var menu = ["americano" : 4500, "iceamericano" : 4500, "americanoice" : 4500, "hotamericano" : 4500, "americanohot" : 4500, "cafelatte" : 5000, "icecafelatte" : 5000, "cafelatteice" : 5000, "hotcafelatte" : 5000, "cafelattehot" : 5000, "anything" : 4500]
 
-    var sum = 0
+//     var sum = 0
 
-    for pro in order {
-        sum += menu[pro] ?? 0
-    }
-    return sum
+//     for pro in order {
+//         sum += menu[pro] ?? 0
+//     }
+//     return sum
+    return order.map { $0.contains("cafelatte") ? 5000 : 4500}.reduce(0, +)
 }
