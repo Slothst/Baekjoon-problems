387d6cda231309654426fbc42ce92a23ae77a0ed
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-01-25 15:06:06 +0900

    [level 0] Title: 외계어 사전, Time: 0.14 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.swift
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 func solution(_ spell:[String], _ dic:[String]) -> Int {
-    for word in dic {
-        var count = 0
-        spell.forEach { alp in
-            if word.contains(alp) {
-                count += 1
-            }
-        }
-        if count == spell.count {
-            return 1
-        }
-    }
-    return 2
+    // for word in dic {
+    //     var count = 0
+    //     spell.forEach { alp in
+    //         if word.contains(alp) {
+    //             count += 1
+    //         }
+    //     }
+    //     if count == spell.count {
+    //         return 1
+    //     }
+    // }
+    // return 2
+    return dic.map { String($0.sorted()) }.contains(spell.sorted().joined()) ? 1 : 2
 }
