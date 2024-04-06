81effdd9bab10c4c19ed7c616d0b2351a1e56f1a
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-06 15:08:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -3,11 +3,7 @@ import Foundation
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     var sum = 0
     for i in 0..<absolutes.count {
-        if !signs[i] {
-            sum += absolutes[i] * -1
-        } else {
-            sum += absolutes[i]
-        }
+        sum += signs[i] ? absolutes[i] : -absolutes[i]
     }
     return sum
 }
