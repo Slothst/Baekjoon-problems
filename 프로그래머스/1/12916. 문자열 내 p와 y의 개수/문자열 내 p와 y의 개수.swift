0cbb673f88dbef97de9bd3dfbe7c6fc421a7f4ae
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-06 13:47:18 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.09 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -1,9 +1,5 @@
 import Foundation
 
-func solution(_ s:String) -> Bool
-{
-    var pCnt = s.map { $0.lowercased() }.filter { $0 == "p" }.count
-    var yCnt = s.map { $0.lowercased() }.filter { $0 == "y" }.count
-    
-    return pCnt == yCnt ? true : false
+func solution(_ s:String) -> Bool {
+    return s.lowercased().filter { $0 == "p" }.count == s.lowercased().filter { $0 == "y" }.count
 }
