544a6f0026c2ee5c96efe74b323a572d1f3163aa
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-08 14:39:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.06 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.swift
@@ -1,11 +1,3 @@
 func solution(_ n:Int) -> String {
-    var ans: String = ""
-    for i in 0..<n {
-        if i % 2 == 0 {
-            ans += "수"
-        } else {
-            ans += "박"
-        }
-    }
-    return ans
+    return (0..<n).map { $0 % 2 == 0 ? "수" : "박"}.reduce("", +)
 }
