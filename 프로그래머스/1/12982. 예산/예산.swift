9891f994ce9e308cd74b10de21eea02de1e927df
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-08 18:00:30 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.swift
+++ 프로그래머스/1/12982. 예산/예산.swift
@@ -2,15 +2,9 @@ import Foundation
 
 func solution(_ d:[Int], _ budget:Int) -> Int {
     var budget = budget
-    var count = 0
-    for i in d.sorted(by: <) {
-        if budget >= i {
-            budget -= i
-            count += 1
-        }
-        if budget <= 0 {
-            break
-        }
-    }
-    return count
+    
+    return d.sorted(by: <).filter {
+        budget -= $0
+        return budget >= 0
+    }.count
 }
