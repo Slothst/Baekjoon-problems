a92cf9203b1236bc507b67b898c589fe3a324f9b
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-08 23:22:41 +0900

    [level 1] Title: K번째수, Time: 0.17 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,10 +1,7 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var result: [Int] = []
-
-    for i in 0..<commands.count {
-        result.append(array[commands[i][0] - 1...commands[i][1] - 1].sorted(by: <)[commands[i][2] - 1])
+    return commands.map {
+        array[($0[0] - 1)...($0[1] - 1)].sorted(by: <)[($0[2] - 1)]
     }
-    return result
 }
