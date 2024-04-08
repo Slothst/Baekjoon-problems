8bba1a1adde86dddfb946394c1c23dc2b2e59e21
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-08 17:19:26 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,12 +1,6 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var n = n
-    var str = ""
-    
-    while n != 0 {
-        str += String(n % 3)
-        n /= 3
-    }
-    return Int((str), radix: 3)!
+    var flipToThree = String(n, radix: 3)
+    return Int(String(flipToThree.reversed()), radix: 3)!
 }
