9bc01e6a845eb60129f6304cf18e68fcb4129766
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-09 16:17:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.28 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=swift) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 0.10 ms
+메모리: 16.5 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 15:40:41
+2024년 04월 09일 16:17:29
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -0,0 +1,32 @@
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    func makeMap(_ arr: [Int], _ n: Int) -> [[String]] {
+        var map = Array(repeating: Array(repeating: " ", count: n), count: n)
+
+        var convertArr = arr.map { String(format: "%0\(n)ld", Int(String($0, radix: 2))!) }
+        for i in 0..<n {
+            for j in 0..<n {
+                if Array(convertArr[i])[j] == "1" {
+                    map[i][j] = "#"
+                }
+            }
+        }
+
+        return map
+    }
+
+    func result(_ map1: [[String]], _ map2: [[String]]) -> [String] {
+        var map = Array(repeating: Array(repeating: " ", count: map1.count), count: map1.count)
+
+        for i in 0..<n {
+            for j in 0..<n {
+                if map1[i][j] == "#" || map2[i][j] == "#" {
+                    map[i][j] = "#"
+                }
+            }
+        }
+
+        return map.map { $0.joined() }
+    }
+
+    return result(makeMap(arr1, n), makeMap(arr2, n))
+}
