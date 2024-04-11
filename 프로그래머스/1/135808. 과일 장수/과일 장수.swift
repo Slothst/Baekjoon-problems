1823b8c4cebb6bd458ef5c3cd9176a725de7b54e
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-11 15:45:13 +0900

    [level 1] Title: 과일 장수, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -3,7 +3,5 @@ import Foundation
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
     var score = score.sorted(by: >)
 
-    var sum = 0
-    stride(from: m - 1, to: score.count, by: m).map { sum += score[$0] * m }
-    return sum
+    return stride(from: m - 1, to: score.count, by: m).map { score[$0] * m }.reduce(0, +)
 }
