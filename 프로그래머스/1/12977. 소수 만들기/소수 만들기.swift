f41a125869adbafc3b1a9eb15fcad822efe1176b
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-11 17:41:59 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -1,20 +1,21 @@
 import Foundation
 
 func solution(_ nums:[Int]) -> Int {
+    func isPrime(_ num: Int) -> Bool {
+        var n = 2
+        while n < num {
+            if num % n == 0 { return false }
+            n += 1
+        }
+        return true
+    }
+    
     var count = 0
     for i in 0..<nums.count - 2 {
         for j in i + 1..<nums.count - 1 {
             for k in j + 1..<nums.count {
                 var sum = nums[i] + nums[j] + nums[k]
-                var cnt = 0
-                for i in 1...sum {
-                    if sum % i == 0 {
-                        cnt += 1
-                    }
-                }
-                if cnt == 2 {
-                    count += 1
-                }
+                if isPrime(sum) { count += 1 }
             }
         }
     }
