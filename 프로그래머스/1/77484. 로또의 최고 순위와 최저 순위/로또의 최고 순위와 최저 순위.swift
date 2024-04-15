804bfb865ed172c1f0fe3c411c45fbe40dc6460a
최낙주(Choi Nakju) <102890326+Slothst@users.noreply.github.com>
2024-04-15 17:09:48 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+    var lottos = lottos
+    var count = 0
+    var minimum = 0
+    var maximum = 0
+    var result: [Int] = []
+
+    func getCounts(_ lottos: [Int], _ win_nums: [Int]) -> Int {
+        var count = 0
+        for num in win_nums {
+            if lottos.contains(num) {
+                count += 1
+            }
+        }
+        return count
+    }
+
+    func getRank(_ count: Int) {
+        if count == 6 {
+            result.append(1)
+        } else if count == 5 {
+            result.append(2)
+        } else if count == 4 {
+            result.append(3)
+        } else if count == 3 {
+            result.append(4)
+        } else if count == 2 {
+            result.append(5)
+        } else {
+            result.append(6)
+        }
+    }
+
+    minimum = getCounts(lottos, win_nums)
+
+    for i in 0..<lottos.count {
+        if lottos[i] == 0 {
+            for num in win_nums {
+                if !lottos.contains(num) {
+                    lottos[i] = num
+                    break
+                }
+            }
+        }
+    }
+
+    maximum = getCounts(lottos, win_nums)
+
+    getRank(maximum)
+    getRank(minimum)
+
+    return result
+}
